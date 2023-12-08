8bd1cdb725593759c0b7466e2a3ca86210e60d5a
jain12swati <swatijain@adobe.com>
2023-12-08 16:17:47 +0530

    data loaded

--- blocks/contributor/contributor.js
+++ blocks/contributor/contributor.js
@@ -1,20 +1,37 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
 /* fetching data for contributors */
- const url = "/contributors.json";
-//const url = `https://main--franklinpractice--jain12swati.hlx.page/contributors.json`;
-/* let x = {};
-fetch(`${pageUrl}`)
-.then(response)
-.then(console.log("success", response));  */
+const url = "/contributors.json";
+let jsonData = "";
+async function getJson(url) {
+    let response = await fetch(url);
+    let data = await response.json()
+    return data;
+}
 
-fetch('url')
-  .then(response => {
-    //handle response            
-    console.log(response);
-  })
-  .then(data => {
-    //handle data
-    console.log(data);
-  })
-  .catch(error => {
-    //handle error
-  });
+
+
+export default async function decorate(block) {
+    jsonData = await getJson(url);    
+    console.log("after call", jsonData);
+
+    let contributorList = jsonData.contributor;
+
+    console.log("hi", contributorList);
+ 
+    const ul = document.createElement('ul');
+    ul.classList.add("contributor-list");
+   
+    (contributorList.data).forEach(el => {
+        const li = document.createElement('li');
+        li.classList.add("contributor-list-item")
+        li.innerHTML = `<div class="contributor-picture"><img src=${el.image_url}></div>
+        <div class="contributor-name">${el.name}</div>
+        <div class="contirbutor-profile">${el.tags}</div>
+        <div class="social-profile">${el.facebook}</div>`;
+
+        ul.append(li);
+    });   
+   
+       block.textContent = '';
+       block.append(ul);
+}
