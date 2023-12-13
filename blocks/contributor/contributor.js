ef672e357055a99b639cba7205a10b5006f470db
jain12swati <swatijain@adobe.com>
2023-12-13 15:24:20 +0530

    removed extra code?

--- blocks/contributor/contributor.js
+++ blocks/contributor/contributor.js
@@ -3,31 +3,18 @@ import { createOptimizedPicture } from '../../scripts/aem.js';
 //const url = "/contributors.json";
 
 let jsonData = "";
-async function getJson(url) {
-    let response = await fetch(url);
-    let data = await response.json()
-    return data;
-}
-
-
-
 export default async function decorate(block) {
-    const url = block.children?.[0]?.children?.[0]?.textContent ;
-    console.log("my url", url);
-  //  jsonData = await getJson(url); 
- //  fetch(url).then(x => x.text());
-   // let data =  response.json()   
-   // console.log("after call", response);
-   const ul = document.createElement('ul');
-   try {
-     const response = await fetch(url);
-     const {data = []} = await response.json(); 
-      ul.classList.add("contributor-list");
-   
+  const url = block.children?.[0]?.children?.[0]?.textContent;
+  const ul = document.createElement('ul');
+  try {
+    const response = await fetch(url);
+    const { data = [] } = await response.json();
+    ul.classList.add("contributor-list");
+
     data.forEach(el => {
-        const li = document.createElement('li');
-        li.classList.add("contributor-list-item")
-        li.innerHTML = `<div class="contributor-picture"><img src=${el.image_url}></div>
+      const li = document.createElement('li');
+      li.classList.add("contributor-list-item")
+      li.innerHTML = `<div class="contributor-picture"><img src=${el.image_url}></div>
         <div class="contributor-name">${el.name}</div>
         <div class="contirbutor-profile">${(el.tags).split(",").join(" | ")}</div>
         <div class="social-profile">
@@ -36,14 +23,11 @@ export default async function decorate(block) {
         <a href="${el.instagram}"><img src="./icons/instagram.png"></a>
         </div>`;
 
-        ul.append(li);
-      });   
-   } catch {
+      ul.append(li);
+    });
+  } catch {
 
-   }
- 
-    
-   
-       block.innerHTML = '';
-       block.appendChild(ul);
+  }
+  block.innerHTML = '';
+  block.appendChild(ul);
 }
